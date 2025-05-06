7ad0fd8a081ad32225dc08672e381fbc58928414
EriPuk19 <eripuk19@htl-shhkoder.com>
2025-05-06 14:04:35 +0200

    Edited application page

--- src/main/java/com/example/bookingsystem/HelloApplication.java
+++ src/main/java/com/example/bookingsystem/HelloApplication.java
@@ -1,19 +1,20 @@
-package com.example.bookingsystem;
+package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Booking Manager");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
